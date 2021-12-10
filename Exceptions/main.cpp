a1bf5c52f213c318dea4c7736f4ee85091948a1c
distoddard <daniel.stoddard@sinclair.edu>
2021-12-10 17:01:00 -0500

    completed the program per the requirements

--- Exceptions/main.cpp
+++ Exceptions/main.cpp
@@ -6,3 +6,38 @@
 #include <cctype>
 #include <cstdlib>
 using namespace std;
+
+char character(char, int);
+
+class invalidCharacterException{};
+class invalidRangeException{};
+
+int main() {
+	const int amt = 5;
+	char letter;
+	char tests[][amt] = { {'a', 'a', 'Z', '?', 'A'},{1, -1, -1, 5, 32} };
+
+	for (int x = 0; x < amt; x++) {
+		try {
+			letter = character(tests[0][x], tests[1][x]);
+			cout << letter << endl;
+		}
+		catch (invalidCharacterException) {
+			cout << "INVALID CHARACTER!" << endl;
+		}
+		catch (invalidRangeException) {
+			cout << "INVALID RANGE!" << endl;
+		}
+	}
+	return 0;
+}
+
+char character(char start, int offset) {
+	if (!isalpha(start)) {
+		throw invalidCharacterException();
+	}
+	else if (!isalpha(tolower(start) + offset)) {
+		throw invalidRangeException();
+	}
+	return start + offset;
+}
